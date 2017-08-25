a9494f79be5894e53d52e022fc70a222d201b0ad
Edno Nunes Ferreira <ednonunes@gmail.com>
2017-08-25 18:08:21 -0300

    Commit inicial

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Chat</title>
+	
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+	<meta name="csrf-token" content="{{ csrf_token() }}">﻿
+</head>
+<body>
+	<h1>Chat</h1>
+
+	<div id="app">
+		<example></example>		
+	</div>
+
+	<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
